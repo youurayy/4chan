30e4b509a3243abad034e247cc6776a3d5207291
ypocat <juraj.vitko@gmail.com>
2012-07-21 15:25:39 +0300

    image 404 fix

--- 4chan.js
+++ 4chan.js
@@ -240,12 +240,19 @@ function downloadPics(ret, cb) {
 			};
 			
 			request(opts, _x(cb, true, function(err, res, body) {
-				if(res.statusCode != 200)
-					cb('Cannot load (status ' + res.statusCode + '): ' + entry.url);
-				fs.writeFile(fname, body, null, _x(cb, true, function(err) {
-					console.log(fname);
+				if(res.statusCode == 404) {
+					console.log('Cannot load (status ' + res.statusCode + '): ' + entry.url);
 					cb();
-				}));
+				}
+				else if(res.statusCode != 200) {
+					cb('Cannot load (status ' + res.statusCode + '): ' + entry.url);
+				}
+				else {
+					fs.writeFile(fname, body, null, _x(cb, true, function(err) {
+						console.log(fname);
+						cb();
+					}));
+				}
 			}));
 			
 		});
